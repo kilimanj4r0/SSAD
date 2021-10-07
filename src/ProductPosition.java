bfb1c59f9733873a946122b5d3b6b244075e046f
Vladimir <v.makharev@yandex.ru>
2021-10-07 22:14:35 +0300

    Apply Code Guideline for ProductPosition class

--- src/ProductPosition.java
+++ src/ProductPosition.java
@@ -1,29 +1,24 @@
-/**
- * Represents the family of identical products. Has type of product and amount of products of this type.
- */
+/** Represents the family of identical products. Has type of product and amount of products of this type. */
 public class ProductPosition {
     private AbstractProduct product;
     private int quantity;
 
-    /**
-     * @return  Total sum of all products of one type
-     */
+    /** Returns the total sum of all products of one type */
     public double calculateSum() {
         return product.getProductPrice() * quantity;
     }
 
-    /**
-     * Changes the quantity of the products of certain type
-     * @param quantity  The new number of products
-     */
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
+    /** Returns the quantity of products of certain type */
+    public int getQuantity() {
+        return quantity;
     }
 
     /**
-     * @return  Quantity of products of certain type
+     * Changes the quantity of the products of certain type
+     *
+     * @param quantity The new number of products
      */
-    public int getQuantity() {
-        return quantity;
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 }
