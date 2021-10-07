f13ebbfa6a74be91834316e12dfb36be0331b617
smasiner <m.shaihutdinov@innopolis.university>
2021-10-07 23:37:22 +0300

    Apply Guideline for ProductPosition class

--- src/ProductPosition.java
+++ src/ProductPosition.java
@@ -1,5 +1,3 @@
-import jdk.internal.org.objectweb.asm.tree.ClassNode;
-
 /**
  * Represents the family of identical products. Has type of product and amount of products of this type.
  */
@@ -7,30 +5,31 @@ public class ProductPosition {
     public AbstractProduct product;
     public int quantity;
 
-    public ProductPosition(AbstractProduct product, int quantity){
+    public ProductPosition(AbstractProduct product, int quantity) {
         this.product = product;
         this.quantity = quantity;
     }
 
     /**
-     * @return  Total sum of all products of one type
+     * @return Total sum of all products of one type
      */
     public double calculateSum() {
         return product.getProductPrice() * quantity;
     }
 
     /**
-     * Changes the quantity of the products of certain type
-     * @param quantity  The new number of products
-     */
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    /**
-     * @return  Quantity of products of certain type
+     * @return Quantity of products of certain type
      */
     public int getQuantity() {
         return quantity;
+    }
+
+    /**
+     * Changes the quantity of the products of certain type
+     *
+     * @param quantity The new number of products
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 }
