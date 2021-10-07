450f1c1f0e8d5b793eff8864bca75933698657df
Vladimir <v.makharev@yandex.ru>
2021-10-08 00:21:54 +0300

    Apply Code Guidelines for Main class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Main {
-    public static void printProductList(ArrayList<ProductPosition> array){
+    public static void printProductList(ArrayList<ProductPosition> array) {
         for (ProductPosition productPosition : array) {
             System.out.println("Id, Name, Price, Number of product: "
                     + productPosition.product.id + ", "
@@ -12,22 +12,22 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Cashbox cashBox = new Cashbox();
-        String[] name_products = new String[]{"Pot", "Arrow", "Stockings"};
-        double[] prices_products = new double[]{123, 344, 23};
+        Cashbox cashbox = new Cashbox();
+        String[] nameProducts = new String[]{"Pot", "Arrow", "Stockings"};
+        double[] pricesProducts = new double[]{123, 344, 23};
 
-        for (int i = 0; i < name_products.length; i++){
-            cashBox.addProduct(new ProductPosition(new ProductType1(name_products[i], prices_products[i]), i + 2));
+        for (int i = 0; i < nameProducts.length; i++) {
+            cashbox.addProduct(new ProductPosition(new ProductType1(nameProducts[i], pricesProducts[i]), i + 2));
         }
 
         Customer client = new Customer(new ProductCart());
         client.name = "Sirojiddin Komolov";
 
-        printProductList(cashBox.getProductList());
+        printProductList(cashbox.getProductList());
 
-        client.productCart.addProduct(cashBox.getProduct(1, 2));
-        client.productCart.addProduct(cashBox.getProduct(1, 14));
+        client.productCart.addProduct(cashbox.getProduct(1, 2));
+        client.productCart.addProduct(cashbox.getProduct(1, 14));
 
-        client.productCart.createOrder(cashBox);
+        client.productCart.createOrder(cashbox);
     }
 }
