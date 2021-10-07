6b21c2eeeb9d3fdaaeb515cb474e24b059321dc1
Vladimir <v.makharev@yandex.ru>
2021-10-07 22:16:23 +0300

    Apply Code Guideline for ProductType1 class

--- src/ProductType1.java
+++ src/ProductType1.java
@@ -1,22 +1,19 @@
-import static java.lang.System.exit;
-
-/**
- * Represents the certain type of product
- */
+/** Represents the certain type of product. */
 public class ProductType1 extends AbstractProduct {
-
     /**
      * Copy constructor
-     * @param target    The product that will be copied
+     *
+     * @param target The product that will be copied
      */
-    public ProductType1(ProductType1 target){
+    public ProductType1(ProductType1 target) {
         super(target);
     }
 
     /**
      * Constructor
-     * @param name      name of the product
-     * @param price     price of the product
+     *
+     * @param name  name of the product
+     * @param price price of the product
      */
     public ProductType1(int id, String name, double price) {
         super(id, name, price);
@@ -24,27 +21,26 @@ public class ProductType1 extends AbstractProduct {
 
     /**
      * Clones the object
-     * @return  clone of the ProductType1
+     *
+     * @return clone of the ProductType1
      */
-    @Override
-    public AbstractProduct makeClone() {
+    @Override public AbstractProduct makeClone() {
         return new ProductType1(this);
     }
 
     /**
      * Function for creating the product via checking the key
-     * @param name      name of the product
-     * @param price     price of the product
-     * @param key       key to get/not get access to the creation of the product
-     * @return          product in case of succeed, null otherwise
+     *
+     * @param name  name of the product
+     * @param price price of the product
+     * @param key   key to get/not get access to the creation of the product
+     * @return product in case of succeed, null otherwise
      */
-    @Override
-    public AbstractProduct createProduct(int id, String name, double price, String key){
-        if (secretKey.equals(key)){
+    @Override public AbstractProduct createProduct(int id, String name, double price, String key) {
+        if (secretKey.equals(key)) {
             System.out.println("You can not create product. Wrong key.");
             return null;
         }
         return new ProductType1(id, name, price);
     }
-
 }
