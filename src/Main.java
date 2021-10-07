1cde6e123182c5728c06c5e4f39a808205ba5d07
smasiner <m.shaihutdinov@innopolis.university>
2021-10-07 18:15:12 +0300

    meme in main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-
-	// write your code here
+        Customer client = new Customer();
+        client.name = "Sirojiddin Komolov";
     }
 }
