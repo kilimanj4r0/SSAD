fb1ba0700089ed29e59156efa191548f3a56c6e3
Vladimir <v.makharev@yandex.ru>
2021-10-07 16:57:30 +0300

    Fill the Cashbox class

--- src/CashBox.java
+++ src/CashBox.java
@@ -1,3 +1,26 @@
-public class CashBox {
+import java.util.ArrayList;
 
+public class CashBox {
+    private static String secretKey;
+    private int id;
+    private ArrayList<ProductPosition> productPositions;
+
+    public void removeProduct(int id) {
+
+    }
+    public void addProduct(ProductPosition productPosition) {
+
+    }
+    public ArrayList<ProductPosition> getProductList() {
+        return productPositions;
+    }
+    public int calculateQuantityOfProducts() {
+        return 0;
+    }
+    public double calculateOverallSum() {
+        return 0.0;
+    }
+    public boolean checkAvailability() {
+        return true;
+    }
 }
