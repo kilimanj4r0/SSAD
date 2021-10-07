31c7e5a4592329abbad9adf98f82d64b82cc88d0
Vladimir <v.makharev@yandex.ru>
2021-10-07 17:05:57 +0300

    Fill the ProductPosition class

--- src/ProductPosition.java
+++ src/ProductPosition.java
@@ -1,3 +1,16 @@
 public class ProductPosition {
+    private AbstractProduct product;
+    private int quantity;
 
+    public double calculateSum() {
+        return 0.0;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
 }
